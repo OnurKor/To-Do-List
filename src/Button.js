bc02064efb359892f2a4dcad2ec26a66a78c8db8
Smarteq-Onur <onurkordogan@gmail.com>
2023-12-16 10:19:36 +0300

    start

--- src/Button.js
+++ src/Button.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
 const Button = ({size, color, func, content, textColor, disable}) => {
+  console.log(`${color}`)
+  console.log(`${size}`)
+  console.log(`${textColor}`)
   return (
-
 <button className={`${size === "sm" && "w-[150px]"} ${size === "md" && "w-[300px]"} ${size === "lg" && "w-full"} border bg-${color}-500 text-${textColor}-300`} onClick={func} disabled={disable}>{content}</button>
   ) 
 }
